54ca1d0cf0e5bd6d4a10886006d7f8e63f1c56d2
ashivs2500 <antonioshivers2500@yahoo.com>
2020-11-06 14:05:15 -0500

    changed some files
    11/06/2020
    edited index.scss to have better styling
    added the images folder to have all the images i will use for this project
    edited index.html to have a better webpage
    edited package and packgae-lock to match the name of my folders

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,9 @@
+curl --include --request PATCH http://localhost:4741/change-password/ \
+  --header "Authorization: Bearer $TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "an example password",
+      "new": "super sekrit"
+    }
+  }'
---
+++ curl-scripts/sign-out.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE http://localhost:4741/sign-out/ \
+  --header "Authorization: Bearer $TOKEN"
